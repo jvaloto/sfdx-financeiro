3ee65a988e6eee0fee565650021540f83e60e3ed
Jonathan Valoto <jonathan.valoto@gmail.com>
2022-02-19 14:37:19 -0300

    new changes - before scratch org

---
+++ Financeiro/main/default/aura/criar_parcelamento/criar_parcelamentoController.js
@@ -0,0 +1,13 @@
+({
+init : function (component) {
+// Find the component whose aura:id is “flowData”
+var flow = component.find("flowData");
+// In that component, start your flow. Reference the flow’s Unique Name.
+flow.startFlow("Criar_parcelamento");
+}
+    , closeThis: function(component, event, helper){
+ var dismissActionPanel = $A.get("e.force:closeQuickAction");
+        dismissActionPanel.fire();
+       
+    }
+})
